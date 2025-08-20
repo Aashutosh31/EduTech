6e25b98104331deb0a39fb9540ea966ea3c7509a
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2025-08-21 00:31:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// EduTech main JS
+// Placeholder for interactivity
+console.log('EduTech loaded');
+
+let instructors = document.getElementById('instructors');
+instructors.addEventListener('click',()=>{
+    window.open('instructors.html');
+})
+
+let batches = document.getElementById('batches');
+batches.addEventListener('click',()=>{
+    window.open('batches.html');
+})
